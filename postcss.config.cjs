0fa571952fa1cf78fb07b47107a6b4eda74cac04
Cesar Fernandez <cfpcreator1@gmail.com>
2025-07-04 16:28:35 -0400

    Fixed packages incompatibilities + changed tailwind @ postcss.config file

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,9 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
-  },
+  }
 }
+
 // This is a PostCSS configuration file that uses Tailwind CSS and Autoprefixer.
 // It specifies that both plugins should be used when processing CSS files.
